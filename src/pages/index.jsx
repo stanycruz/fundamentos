312083602a30e7783aedae7b5311af49e5dc7a2e
Stanislaw Cruz <stanycruz@outlook.com>
2021-11-10 13:42:41 -0300

    Entendendo o JSX

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,8 @@
 export default function Inicio() {
-    return 'Início 2'
+    return (
+        <div>
+            <h1>Fundamentos do Next.js & React</h1>
+            <h2>Vamos estudar esse framework</h2>
+        </div>
+    )
 }
