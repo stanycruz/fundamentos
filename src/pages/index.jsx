3d6df871b347d5202e0cf5fc0ca35b641ee4e175
Stanislaw Cruz <stanycruz@outlook.com>
2021-11-18 12:14:21 -0300

    Integração com API

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,7 +15,8 @@ export default function Inicio() {
             <Navegador texto="JSX" destino="/jsx" cor="crimson" />
             <Navegador texto="Navegação #01" destino="/navegacao" cor="green" />
             <Navegador texto="Navegação #02" destino="/cliente/sp-2/123" cor="blue" />
-            <Navegador texto="Componente com Estado" destino="/estado" cor="pink" />
+            <Navegador texto="Componente com Estado" destino="/estado" cor="palevioletred" />
+            <Navegador texto="Integração com API #01" destino="/integracao_1" cor="peru" />
         </div>
     )
 }
